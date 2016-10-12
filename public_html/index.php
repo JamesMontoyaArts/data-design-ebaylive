d462ce1e95a7f53d95d8b931f21961c04b22db1f
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-12 13:40:23 -0600

    Created persona and use case

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,48 @@
 <!DOCTYPE html>
 <html>
 	<head>eBay Live</head>
-	<body></body>
+	<body>
+
+		<h1>Persona</h1>
+
+		<p><b>Name</b>: Carlos Contreas</p>
+
+		<p><b>Age</b>: 33 </p>
+
+		<p><b>Profession</b> Full time tax auditor for the city of Albuquerque.  He recently recieved an unexpected bonus and has decided to use it to purchase more memorabillia to add to his collection.</p>
+
+		<p><b>Technology</b>: Macbook pro 15" laptop with retina display and a 30" hi-def Samsung television at home. Broadband internet through the devil aka Comcast</p>
+
+		<p><b>Attitudes and Behaviors</b>: He is an avid collector who searches far and wide both on line and the real world to finish his collection of original 1970's star wars action figures.  His fandom is in every corner of his life.  He has Stara Wars socks, Star Wars sheets on his bed, Darth Vader bath soap and he even had a Star Wars theme wedding with his bride wearing the iconic Leia buns. </p>
+
+		<p><b>Needs and Frustrations</b>: He is looking to buy original Star Wars memorablilia that is missing from his collection.  He enjoys the convenience of bidding online because it is hard to find a lot of the original merchandise he is looking for here in Albuquerque. He has bid on several online auctions and has used ebay for several of his memorabillia purchases. He has been to several live auctions in person but he is new to "live" online auctions. He is intrigued by the original Star Wars props that are for sale in an upcoming eBay Live auction. While it is convinient to bid online he hates when he loses an auction because he was busy and couldn't up his bid in time. He hopes that "live" eBay auctions will eliminate this problem because he will be watching it happen "live".</p>
+
+		<h2>Use Case</h2>
+
+		<ul>
+
+			<li>Visits eBay Live in hopes of winning some coveted items</li>
+			<li>Registers to be a bidder in the upcoming Star Wars: Original Props and Collectables From The Set of Empire Strikes Back auction</li>
+			<li>While waiting for the bidding to start he peruses the catalog noting the lots he wants to bid on</li>
+			<li>Watches in anticipation as lots become "active", still waiting for the certain lots that he hopes will be his by the end of it</li>
+			<li>He places bids as the lots he's been waiting for become active.</li>
+			<li>Bidding and re-bidding as the price of the original luke hand that gets cut off continues to rise in value</li>
+			<li>Watches price go up with each consecutive bid</li>
+			<li>Changes his bid to an outragous price hoping to clingh the lot</li>
+			<li>Yells at the screen for his bid to be the highest</li>
+			<li>Bites nails in anticipation of winning or losing</li>
+			<li>Either wins or loses auction based upon his maximum bid</li>
+
+		</ul>
+
+		<h3>Entities</h3>
+
+		<ul>
+
+			<li>Event catalog</li>
+			<li>Auctioner company info</li>
+
+		</ul>
+
+	</body>
 </html>
