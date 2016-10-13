a0492ccc574cda5f658bba265b9b2e90c479e2a6
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-13 15:41:49 -0600

    fixed more typos and changed cat to catalog

--- public_html/index.php
+++ public_html/index.php
@@ -14,17 +14,17 @@
 
 		<p><b>Age</b>: 33 </p>
 
-		<p><b>Profession</b> Full time tax auditor for the city of Albuquerque.  He recently recieved an unexpected bonus and has decided to use it to purchase more memorabillia to add to his collection.</p>
+		<p><b>Profession</b> Full time tax auditor for the city of Albuquerque.  He recently received an unexpected bonus and has decided to use it to purchase more memorabilia to add to his collection.</p>
 
 		<p><b>Technology</b>: Macbook pro 15" laptop with retina display, iPhone 5 and a 30" hi-def Samsung television at home. Broadband internet through the devil aka Comcast</p>
 
 		<p><b>Attitudes and Behaviors</b>: He is an avid collector who searches far and wide both on line and the real world to finish his collection of original 1970's star wars action figures.  He scours sites like eBay almost daily to find missing items in the collection.  He even goes so far as to keep checking his phone, even during meetings at work, when certain auctions were getting close to their end.  His fandom penetrates every corner of his life.  He has Star Wars socks, Star Wars sheets on his bed, Darth Vader bath soap and he even had a Star Wars theme wedding with his bride wearing the iconic Leia buns.</p>
 
-		<p><b>Needs and Frustrations</b>: He is looking to buy original Star Wars memorablilia that is missing from his collection.  He enjoys the convenience of bidding online because it is hard to find a lot of the original merchandise he is looking for here in Albuquerque. He has bid on several online auctions and has used ebay for several of his memorabillia purchases. He has been to several live auctions in person but he is new to "live" online auctions. He is intrigued by the original Star Wars props that are for sale in an upcoming eBay Live auction. While it is convinient to bid online he hates when he loses an auction because he was busy and couldn't up his bid in time. He hopes that "live" eBay auctions will eliminate this problem because he will be watching it happen "live".  </p>
+		<p><b>Needs and Frustrations</b>: He is looking to buy original Star Wars memorabilia that is missing from his collection.  He enjoys the convenience of bidding online because it is hard to find a lot of the original merchandise he is looking for here in Albuquerque. He has bid on several online auctions and has used ebay for several of his memorabilia purchases. He has been to several live auctions in person but he is new to "live" online auctions. He is intrigued by the original Star Wars props that are for sale in an upcoming eBay Live auction. While it is convenient to bid online he hates when he loses an auction because he was busy and couldn't up his bid in time. He hopes that "live" eBay auctions will eliminate this problem because he will be watching it happen "live".  </p>
 
 		<h2>Use Case</h2>
 
-			<p>Carlos has been looking for original production props to elivate his top of the line collection so he searches for Star Wars on eBay.  He notices that in a few days there is going to be an auction of original Star Wars: Empire Strikes Back props and memorabilia.  He is really excited and even takes the day off of work so he can pay attention to the auction without any distractions because he is determined to win a number of long sought after items and hoping to get one or two BIG purchases.</p>
+			<p>Carlos has been looking for original production props to elevate his top of the line collection so he searches for Star Wars on eBay.  He notices that in a few days there is going to be an auction of original Star Wars: Empire Strikes Back props and memorabilia.  He is really excited and even takes the day off of work so he can pay attention to the auction without any distractions because he is determined to win a number of long sought after items and hoping to get one or two BIG purchases.</p>
 
 			<p>The day of the auction arrives and Carlos gets everything ready for bidding.  He boots up his laptop, registers for the auction, grabs a cup of coffee and waits in anticipation for the auction to begin.</p>
 
@@ -41,10 +41,10 @@
 			<li>While waiting for the bidding to start he peruses the catalog noting the lots he wants to bid on by clicking the forward button on the gallery window</li>
 			<li>Watches in anticipation as lots become "active" in the current lot window, still waiting for the certain lots that he hopes will be his by the end of it</li>
 			<li>He places bids as the lots he's been waiting for become active by clicking the bid now button</li>
-			<li>The bid price is increased based upon the incriments set by eBay</li>
-			<li>He clicks again and again, bidding and re-bidding as the price of the original luke hand that gets cut off continues to rise in value</li>
+			<li>The bid price is increased based upon the increments set by eBay</li>
+			<li>He clicks again and again, bidding and re-bidding as the price of the original Luke hand that gets cut off continues to rise in value</li>
 			<li>Watches price go up with each consecutive bid</li>
-			<li>Clicks the bid now button to bid an outragous price hoping to win the lot</li>
+			<li>Clicks the bid now button to bid an outrageous price hoping to win the lot</li>
 			<li>Yells at the screen for his bid to be the highest</li>
 			<li>Bites nails in anticipation of winning or losing</li>
 			<li>Either wins or loses auction based upon his maximum bid</li>
@@ -65,11 +65,11 @@
 					<li>lotStartBid</li>
 					<li>lotValueEstimate</li>
 					<li>lotDiscription</li>
-					<li>catId</li>
+					<li>lotCatalogId</li>
 
 				</ul>
 
-			<li>Auctioner Company Profile</li>
+			<li>Company Profile</li>
 
 				<ul>
 
@@ -77,8 +77,8 @@
 					<li>companyLogo</li>
 					<li>companyLocation</li>
 					<li>companyBuyerPremium</li>
-					<li>shippingPolicy</li>
-					<li>paymentPolicy</li>
+					<li>companyShippingPolicy</li>
+					<li>companyPaymentPolicy</li>
 
 				</ul>
 
@@ -86,8 +86,8 @@
 
 				<ul>
 
-					<li>catId</li>
-					<li>catName</li>
+					<li>catalogId</li>
+					<li>catalogName</li>
 					<li>companyId</li>
 
 				</ul>
