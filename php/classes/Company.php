3871a42a08bc0664855b5ed89599b1b774a60c5d
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-21 11:58:46 -0600

    added mutator methods

--- php/classes/Company.php
+++ php/classes/Company.php
@@ -58,6 +58,23 @@ class Company {
 	}
 
 	/**
+	 * mutator method for company Id
+	 *
+	 * @param int $newCompanyId new value of company Id
+	 * @throws UnexpectedValueException if $newCompanyId is not an integer
+	 */
+	public function setCompanyId($newCompanyId) {
+		$newCompanyId = filter_var($newCompanyId, FILTER_VALIDATE_INT);
+		if($newCompanyId === false)	{
+			throw(new UnexpectedValueException("Company Id is not a valid integer"));
+		}
+
+		//Convert and store the company id
+		$this->companyId = intval($newCompanyId);
+	}
+
+
+	/**
  	* accessor method for company logo
  	*
  	* @return string of company logo
@@ -67,12 +84,45 @@ class Company {
 	}
 
 	/**
- * accessor method for company address
- *
- * @return string for company address
- */
+	 * mutator method for company logo
+	 *
+	 * @param string $newCompanyLogo new string of company logo
+	 * @throws UnexpectedValueException if $newCompanyLogo is not a string
+	 */
+	public function setCompanyLogo($newCompanyLogo) {
+		$newCompanyLogo = filter_input($newCompanyLogo, FILTER_SANITIZE_STRING);
+		if($newCompanyLogo === false)	{
+			throw(new UnexpectedValueException("Company Logo not valid"));
+		}
+
+		//Convert and store the company logo
+		$this->companyLogo = imagestring($newCompanyLogo);
+	}
+
+
+	/**
+ 	* accessor method for company address
+ 	*
+ 	* @return string for company address
+ 	*/
 	public function getCompanyAddress() {
 		return $this->companyAddress;
+	}
+
+	/**
+	 * mutator method for company address
+	 *
+	 * @param string $newCompanyAddress new string of company address
+	 * @throws UnexpectedValueException if $newCompanyAddress is not a string
+	 */
+	public function setCompanyAddress($newCompanyAdress) {
+		$newCompanyAddress = filter_input($newCompanyAddress, FILTER_SANITIZE_STRING);
+		if($newCompanyAddress === false)	{
+			throw(new UnexpectedValueException("Company Address Invalid"));
+		}
+
+		//Convert and store the company address
+		$this->companyAddress = string($newCompanyAddress);
 	}
 
 	/**
@@ -85,6 +135,22 @@ class Company {
 	}
 
 	/**
+	 * mutator method for company buyer premium
+	 *
+	 * @param int $newCompanyBuyerPremium new value of company buyer premium
+	 * @throws UnexpectedValueException if $newCompanyBuyerPremium is not an integer
+	 */
+	public function setCompanyPremium($newCompanyBuyerPremium) {
+		$newCompanyBuyerPremium = filter_var($newCompanyBuyerPremium, FILTER_VALIDATE_INT);
+		if($newCompanyBuyerPremium === false)	{
+			throw(new UnexpectedValueException("Company Buyer Premium is not a valid integer"));
+		}
+
+		//Convert and store the company buyer premium
+		$this->companyBuyerPremium = intval($newCompanyBuyerPremium);
+	}
+
+	/**
 	 * accessor method for company shipping policy
 	 *
 	 * @return string for company shipping policy
@@ -94,11 +160,43 @@ class Company {
 	}
 
 	/**
+	 * mutator method for company shipping policy
+	 *
+	 * @param string $newCompanyShippingPolicy new string of company policy
+	 * @throws UnexpectedValueException if $newCompanyShippingPolicy is not a string
+	 */
+	public function setCompanyShippingPolicy($newCompanyShippingPolicy) {
+		$newCompanyShippingPolicy = filter_input($newCompanyShippingPolicy, FILTER_SANITIZE_STRING);
+		if($newCompanyShippingPolicy === false) {
+			throw(new UnexpectedValueException("Company Shipping Policy Invalid"));
+		}
+
+		//Convert and store the company shipping policy
+		return $this->companyShippingPolicy;
+	}
+
+	/**
 	 * accessor method for company payment policy
 	 *
 	 * @return string for company payment policy
 	 */
 	public function getCompanyPaymentPolicy() {
+		return $this->companyPaymentPolicy;
+	}
+
+	/**
+	 * mutator method for company payment policy
+	 *
+	 * @param string $newCompanyPaymentPolicy new string of company payment
+	 * @throws UnexpectedValueException if $newCompanyPaymentPolicy is not a string
+	 */
+	public function setCompanyPaymentPolicy($newCompanyPaymentPolicy) {
+		$newCompanyPaymentPolicy = filter_input($newCompanyPaymentPolicy, FILTER_SANITIZE_STRING);
+		if($newCompanyPaymentPolicy === false) {
+			throw(new UnexpectedValueException("Company Payment Policy Invalid"));
+		}
+
+		//Convert and store the company payment policy
 		return $this->companyPaymentPolicy;
 	}
 }
